b600f273ce2a54530694ef910270615a161e9dc7
EdwinCB99 <109781851+EdwinCB99@users.noreply.github.com>
2024-08-18 13:11:32 -0600

    Initial commit

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import java.util.List;
+
+
+public interface CategoriaService {
+    //Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+    
+    //Se obtiene una categoria por medio de su id
+    public Categoria getCategoria(Categoria categoria);
+    
+   //metodo guardar
+    public void save(Categoria categoria);
+
+    //metodo eliminar
+    public void delete(Categoria categoria);
+    
+    
+    
+    
+    
+    
+}
